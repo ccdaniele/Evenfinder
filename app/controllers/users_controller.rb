15c13c81d6e233a149eb4245473a782f975d2caf
TheNanoBay <noorani.mehdi@outlook.com>
2021-01-26 21:40:41 -0600

    applied mehdi changes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+    
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def edit 
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        #byebug
+        
+        @user.update(user_params(:name, :age, :location))
+
+        redirect_to user_path(@user)
+    end
+
+
+
+    def user_params(*args)
+        params.require(:user).permit(*args)
+    end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   
   resources :events
+  resources :activities 
   root to: 'pages#home'
-  devise_for :users
+  
+  devise_for :user
+  resources :users, :only => [:show, :edit, :update]
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
